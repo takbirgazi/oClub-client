bd5527def1b86af44e707b40b4faf361a7b9bf5f
takbirgazi <takbirgazibd@gmail.com>
2024-09-06 23:45:18 +0600

    Update Image Url

--- src/Components/DonorList/DonorList.jsx
+++ src/Components/DonorList/DonorList.jsx
@@ -5,7 +5,7 @@ const DonorList = () => {
         {
             id: 1,
             name: "Md. Takbir Gazi",
-            image: "https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_ohc=cFPZlbU9iCcQ7kNvgEl1OdC&_nc_ht=scontent.fjsr12-1.fna&_nc_gid=ALxnD7JdcwLmb9_g0fxAZ-L&oh=00_AYB6n9aO71yyJLPtEKy21pDrXErpaeTn8m-v-KEJr2JzjA&oe=66D53213",
+            image: `https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_eui2=AeHUTZWNH3bT9yoM5tC9RJERIWQ5NtBFCSohZDk20EUJKpq_eaRZgbMQf_272puvAb60sSTEx2PxOSVX23UsjFhm&_nc_ohc=VRmzaXS-Ga0Q7kNvgFwpdFv&_nc_ht=scontent.fjsr12-1.fna&oh=00_AYDE67Zp6OlcbPoMDw5b9UE-HapX0ezO4kmD_5v3DOJbFg&oe=66E10F93`,
             bloodGroup: "O+",
             lastDonate: "24/08/2024",
             location: "Khulna, Bangladesh",
@@ -14,7 +14,7 @@ const DonorList = () => {
         {
             id: 2,
             name: "Md. Takbir Gazi",
-            image: "https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_ohc=cFPZlbU9iCcQ7kNvgEl1OdC&_nc_ht=scontent.fjsr12-1.fna&_nc_gid=ALxnD7JdcwLmb9_g0fxAZ-L&oh=00_AYB6n9aO71yyJLPtEKy21pDrXErpaeTn8m-v-KEJr2JzjA&oe=66D53213",
+            image: `https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_eui2=AeHUTZWNH3bT9yoM5tC9RJERIWQ5NtBFCSohZDk20EUJKpq_eaRZgbMQf_272puvAb60sSTEx2PxOSVX23UsjFhm&_nc_ohc=VRmzaXS-Ga0Q7kNvgFwpdFv&_nc_ht=scontent.fjsr12-1.fna&oh=00_AYDE67Zp6OlcbPoMDw5b9UE-HapX0ezO4kmD_5v3DOJbFg&oe=66E10F93`,
             bloodGroup: "O+",
             lastDonate: "24/08/2024",
             location: "Khulna, Bangladesh",
@@ -23,7 +23,7 @@ const DonorList = () => {
         {
             id: 3,
             name: "Md. Takbir Gazi",
-            image: "https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_ohc=cFPZlbU9iCcQ7kNvgEl1OdC&_nc_ht=scontent.fjsr12-1.fna&_nc_gid=ALxnD7JdcwLmb9_g0fxAZ-L&oh=00_AYB6n9aO71yyJLPtEKy21pDrXErpaeTn8m-v-KEJr2JzjA&oe=66D53213",
+            image: `https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_eui2=AeHUTZWNH3bT9yoM5tC9RJERIWQ5NtBFCSohZDk20EUJKpq_eaRZgbMQf_272puvAb60sSTEx2PxOSVX23UsjFhm&_nc_ohc=VRmzaXS-Ga0Q7kNvgFwpdFv&_nc_ht=scontent.fjsr12-1.fna&oh=00_AYDE67Zp6OlcbPoMDw5b9UE-HapX0ezO4kmD_5v3DOJbFg&oe=66E10F93`,
             bloodGroup: "O+",
             lastDonate: "24/08/2024",
             location: "Khulna, Bangladesh",
@@ -32,7 +32,7 @@ const DonorList = () => {
         {
             id: 4,
             name: "Md. Takbir Gazi",
-            image: "https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_ohc=cFPZlbU9iCcQ7kNvgEl1OdC&_nc_ht=scontent.fjsr12-1.fna&_nc_gid=ALxnD7JdcwLmb9_g0fxAZ-L&oh=00_AYB6n9aO71yyJLPtEKy21pDrXErpaeTn8m-v-KEJr2JzjA&oe=66D53213",
+            image: `https://scontent.fjsr12-1.fna.fbcdn.net/v/t39.30808-1/339843164_762183158612999_6871029539477375640_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=50d2ac&_nc_eui2=AeHUTZWNH3bT9yoM5tC9RJERIWQ5NtBFCSohZDk20EUJKpq_eaRZgbMQf_272puvAb60sSTEx2PxOSVX23UsjFhm&_nc_ohc=VRmzaXS-Ga0Q7kNvgFwpdFv&_nc_ht=scontent.fjsr12-1.fna&oh=00_AYDE67Zp6OlcbPoMDw5b9UE-HapX0ezO4kmD_5v3DOJbFg&oe=66E10F93`,
             bloodGroup: "O+",
             lastDonate: "24/08/2024",
             location: "Khulna, Bangladesh",
