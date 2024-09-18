c7faf87219bdfc6bfc7b5698abf9b7878bb22f0d
takbirgazi <takbirgazibd@gmail.com>
2024-09-19 02:14:25 +0600

    Update: Signe up page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const SignUp = () => {
+    const [message, setMessage] = useState('');
     const handlerSignUp = event => {
         event.preventDefault();
         const form = event.target;
@@ -10,9 +12,14 @@ const SignUp = () => {
         const password = form.password.value;
         const confirmPassword = form.confirmPassword.value;
         const bloodGroup = form.bloodGroup.value;
-        const userData = { fullName, phoneNumber, password, confirmPassword, bloodGroup }
-        axios.post(`${import.meta.env.VITE_base_api}/users`, { userData })
-            .then(res => console.log(res.data))
+        const userData = { fullName, phoneNumber, password, bloodGroup };
+        if (password === confirmPassword) {
+            axios.post(`${import.meta.env.VITE_base_api}/users`, userData)
+                .then(res => {
+                    setMessage(res.data.message);
+                    form.reset();
+                })
+        }
     }
 
     return (
@@ -22,6 +29,7 @@ const SignUp = () => {
                     <h2 className="text-4xl font-bold text-gray-900 pb-5">Sign Up</h2>
                     <div className="flex flex-col gap-2">
                         <form onSubmit={handlerSignUp} className="flex flex-col gap-3">
+                            <div className="text-green-500">{message && message}</div>
                             <div className="flex flex-col gap-1">
                                 <label htmlFor="fullName">Name:</label>
                                 <input className="border border-gray-500 rounded-md p-2 outline-none" type="text" name="fullName" placeholder="Write Your Full Name" required />
@@ -31,15 +39,15 @@ const SignUp = () => {
                                 <input className="border border-gray-500 rounded-md p-2 outline-none" type="text" name="phoneNumber" placeholder="Write Your Phone Number" required />
                             </div>
                             <div className="flex flex-col gap-1">
-                                <label htmlFor="phoneNumber">Password:</label>
+                                <label htmlFor="password">Password:</label>
                                 <input className="border border-gray-500 rounded-md p-2 outline-none" type="password" name="password" placeholder="Set Your Password" required />
                             </div>
                             <div className="flex flex-col gap-1">
-                                <label htmlFor="phoneNumber">Confirm Password:</label>
+                                <label htmlFor="confirmPassword">Confirm Password:</label>
                                 <input className="border border-gray-500 rounded-md p-2 outline-none" type="password" name="confirmPassword" placeholder="Write Your Password" required />
                             </div>
                             <div className="flex flex-col gap-1">
-                                <label htmlFor="phoneNumber">Select Your Blood Group:</label>
+                                <label htmlFor="bloodGroup">Select Your Blood Group:</label>
                                 <select className="border border-gray-500 rounded-md p-2 outline-none" name="bloodGroup" defaultValue="selectGroup" >
                                     <option value="selectGroup" disabled>Select Group</option>
                                     <option value="O+">O+</option>
@@ -53,7 +61,7 @@ const SignUp = () => {
                                 </select>
                             </div>
                             <div className="flex">
-                                <input className="border border-red-500 rounded-md p-2 outline-none cursor-pointer bg-red-800 text-gray-100 font-bold" type="submit" name="phoneNumber" value="Sign Up" />
+                                <input className="border border-red-500 rounded-md p-2 outline-none cursor-pointer bg-red-800 text-gray-100 font-bold" type="submit" name="submit" value="Sign Up" />
                             </div>
                         </form>
                         <NavLink className="text-blue-500" to="/login">I have an account</NavLink>
